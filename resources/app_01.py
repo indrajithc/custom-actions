ffd4143a8e485791471745712fa555829b321b83
indran <indajithc@hotmail.com>
2019-09-09 02:15:18 +0530

    drop event

--- resources/app_01.py
+++ resources/app_01.py
@@ -13,21 +13,90 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(719, 478)
-        self.textBrowser = QtWidgets.QTextBrowser(Dialog)
-        self.textBrowser.setGeometry(QtCore.QRect(170, 80, 331, 51))
-        self.textBrowser.setObjectName("textBrowser")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(300, 200, 80, 22))
+        Dialog.resize(759, 575)
+        self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 50, 631, 401))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.widget = QtWidgets.QWidget(self.verticalLayoutWidget)
+        self.widget.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.widget.setObjectName("widget")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.widget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 651, 31))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.hboxlayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.hboxlayout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        self.hboxlayout.setContentsMargins(0, 0, 0, 0)
+        self.hboxlayout.setObjectName("hboxlayout")
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.pushButton.setObjectName("pushButton")
+        self.hboxlayout.addWidget(self.pushButton)
+        self.verticalLayout.addWidget(self.widget)
+        self.widget_2 = QtWidgets.QWidget(self.verticalLayoutWidget)
+        self.widget_2.setObjectName("widget_2")
+        self.mainContentTab = QtWidgets.QTabWidget(self.widget_2)
+        self.mainContentTab.setGeometry(QtCore.QRect(-4, -1, 641, 361))
+        self.mainContentTab.setObjectName("mainContentTab")
+        self.tab01 = QtWidgets.QWidget()
+        self.tab01.setObjectName("tab01")
+        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.tab01)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(-1, -1, 631, 331))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.widget_3 = QtWidgets.QWidget(self.horizontalLayoutWidget_2)
+        self.widget_3.setEnabled(True)
+        self.widget_3.setMinimumSize(QtCore.QSize(50, 0))
+        self.widget_3.setObjectName("widget_3")
+        self.widget_4 = QtWidgets.QWidget(self.widget_3)
+        self.widget_4.setEnabled(True)
+        self.widget_4.setGeometry(QtCore.QRect(0, 0, 629, 329))
+        self.widget_4.setMinimumSize(QtCore.QSize(50, 0))
+        self.widget_4.setObjectName("widget_4")
+        self.horizontalLayout.addWidget(self.widget_3)
+        self.horizontalLayoutWidget_3 = QtWidgets.QWidget(self.tab01)
+        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(0, 0, 631, 331))
+        self.horizontalLayoutWidget_3.setObjectName("horizontalLayoutWidget_3")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_3)
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.widget_5 = QtWidgets.QWidget(self.horizontalLayoutWidget_3)
+        self.widget_5.setEnabled(True)
+        self.widget_5.setMinimumSize(QtCore.QSize(50, 0))
+        self.widget_5.setObjectName("widget_5")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.widget_5)
+        self.plainTextEdit.setGeometry(QtCore.QRect(160, 60, 104, 70))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.horizontalLayout_3.addWidget(self.widget_5)
+        self.mainContentTab.addTab(self.tab01, "")
+        self.tab02 = QtWidgets.QWidget()
+        self.tab02.setObjectName("tab02")
+        self.mainContentTab.addTab(self.tab02, "")
+        self.verticalLayout.addWidget(self.widget_2)
 
         self.retranslateUi(Dialog)
+        self.mainContentTab.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "file"))
+        self.pushButton.setText(_translate("Dialog", "PushButton"))
+        self.plainTextEdit.setPlainText(_translate("Dialog", "dfsdf sdf sdf\n"
+"sd\n"
+"f \n"
+"sdf\n"
+"sdf\n"
+"sdf\n"
+"sd\n"
+"fs\n"
+"dfsdf \n"
+""))
+        self.mainContentTab.setTabText(self.mainContentTab.indexOf(self.tab01), _translate("Dialog", "Tab 1"))
+        self.mainContentTab.setTabText(self.mainContentTab.indexOf(self.tab02), _translate("Dialog", "Tab 2"))
 
 
 if __name__ == "__main__":
--- test_app_01.py
+++ test_app_01.py
@@ -10,64 +10,243 @@ MIN_HEIGHT = 500
 APP_NAME = "Work"
 DB_PATH = "./data/db"
 
+
+
+
+
+
+
+
+
+
+#  for drag event component 
+class DropComponent(QtWidgets.QLabel): 
+    def __init__(self, type, supera, parent=None):
+        super(DropComponent, self).__init__(parent)
+        self.super = supera 
+
+        color = QtGui.QColor(*[random.randint(0, 255) for _ in range(3)])
+        self.setStyleSheet("background-color: {}".format(color.name()))
+ 
+        self.setMinimumSize(10, 10)
+        self.setGeometry(QtCore.QRect( 0, 0, 20, 20))
+
+        self.setAcceptDrops(True)
+        
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+            links = []
+            for url in event.mimeData().urls():
+                links.append(str(url.toLocalFile()))
+            self.super( links )
+        else:
+            event.ignore()
+
+
+
+# main widget class 
 class Ui_Dialog(QtWidgets.QWidget):
+    # constructor that actually reads current this as patent componet this
     def __init__(self, suself, parent=None):
         super(Ui_Dialog, self).__init__(parent)
          
+        #  set current comonet super as parent scope/ this
         self.super = suself
 
+        # set the minimum with and height
         self.setMinimumSize(MIN_WIDTH, MIN_HEIGHT)
+        # set widget name
         self.setWindowTitle( APP_NAME )
+ 
 
-        lay = QtWidgets.QHBoxLayout(self)
+        # create the vertical box to the widget
+        lay = QtWidgets.QGridLayout(self) 
+
+        self.widget = QtWidgets.QWidget(self)
+        self.widget.setObjectName("top_menu") 
+        self.widget.setMaximumSize(QtCore.QSize(16777215, 30))
+
+        self.menuItemsContainer = QtWidgets.QWidget(self.widget)
+        self.menuItemsContainer.setObjectName("menu_items")
+
         
-
+        
+        self.menuItems = QtWidgets.QHBoxLayout(self.menuItemsContainer)
+        self.menuItems.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        self.menuItems.setContentsMargins(0, 0, 0, 0)
+        self.menuItems.setObjectName("menuItems")
+        
+        #  button ===================== 01
+        self.pushButton01 = QtWidgets.QPushButton(self.menuItemsContainer)
+        self.pushButton01.setObjectName("pushButton01")
+        self.pushButton01.setMaximumSize(70, 25)
+        self.pushButton01.setText("add one ") 
+        self.pushButton01.clicked.connect(self.event_clicked_button01) 
+        self.menuItems.addWidget(self.pushButton01)
+        # ======================================================== end 01
  
         
-        for letter in "ABCDEFG":
-            label = QtWidgets.QLabel(letter, alignment=QtCore.Qt.AlignCenter)
-            color = QtGui.QColor(*[random.randint(0, 255) for _ in range(3)])
-            label.setStyleSheet("background-color: {}".format(color.name()))
-            lay.addWidget(label)
+ 
+        # setting menu items arrange ===> margin-right auto adjest
+        self.qLabel = QtWidgets.QLabel(self.menuItemsContainer)
+        self.qLabel.setObjectName("res_event")
+        self.menuItems.addWidget(self.qLabel)
+
+
 
  
+ 
+        lay.addWidget( self.widget )
+
+
+        
+
+
+
+
+        self.widget_2 = QtWidgets.QWidget(self)
+        self.widget_2.setObjectName("widget_2")
+        self.mainContentTab = QtWidgets.QTabWidget(self.widget_2)
+
+        self.mainContentTab.setObjectName("mainContentTab")
+        self.tab01 = QtWidgets.QWidget()
+        self.tab01.setObjectName("tab01")
+        
+        self.inActionTab01 = QtWidgets.QWidget(self.tab01)
+        self.inActionTab01.setObjectName("inActionTab01")
+
+        self.inTab01Layout = QtWidgets.QHBoxLayout(self.inActionTab01)
+        self.inTab01Layout.setContentsMargins(0, 0, 0, 0)
+        self.inTab01Layout.setObjectName("inTab01Layout")
+
+
+        self.inside_01 = QtWidgets.QWidget(self.inActionTab01)
+        self.inside_01.setObjectName("inside_01") 
+
+        self.inTab01Layout.addWidget(self.inside_01)
+
+
+        self.dropZone = DropComponent( self.inside_01, self.pictureDropped )
+
+        self.inTab01LayoutIn = QtWidgets.QHBoxLayout(self.inside_01)
+        self.inTab01LayoutIn.setContentsMargins(0, 0, 0, 0)
+        self.inTab01LayoutIn.setObjectName("inTab01LayoutIn")
+        
+        # self.connect(self.dropZone, QtCore.SIGNAL("dropped"), self.pictureDropped)
+        self.inTab01LayoutIn.addWidget(self.dropZone)
+
+
+        self.inside_02 = QtWidgets.QWidget(self.inActionTab01)
+        self.inside_02.setObjectName("inside_02")
+        self.inTab01Layout.addWidget(self.inside_02)
+
+
+
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.inside_02)
+        self.plainTextEdit.setObjectName("outputArea") 
+        self.plainTextEdit.setPlainText("plainTextEdit") 
+        self.plainTextEdit.setReadOnly(True)
+        
+        
+
+
+
+        self.mainContentTab.addTab(self.tab01, "action 1")
+
+
+        self.tab02 = QtWidgets.QWidget()
+        self.tab02.setObjectName("tab02")
+        self.mainContentTab.addTab(self.tab02, "action 2")
+ 
+        
+        lay.addWidget(self.widget_2 )
+
+
+
+
+
+        # color = QtGui.QColor(*[random.randint(0, 255) for _ in range(3)])
+        # self.menuItemsContainer.setStyleSheet("background-color: {}".format(color.name()))
+
+    def pictureDropped(self, l):
+        print(l)
+
+ 
+    # trigger when window size is changed, updateing all responsive widgets 
+    def responsive_init( self, event ) :  
+
+        self.menuItemsContainer.setGeometry(QtCore.QRect(0, 0, self.widget.width(), 31)) 
+        self.mainContentTab.setGeometry(QtCore.QRect( 0, 0, self.widget_2.width(), self.widget_2.height()))
+        self.inActionTab01.setGeometry(QtCore.QRect( 0, 0,  self.widget_2.width(), self.widget_2.height()  ))
+        
+        self.inTab01Layout.setGeometry(QtCore.QRect( 0, 0,  self.inActionTab01.width(), self.inActionTab01.height()  ))
+        self.plainTextEdit.setGeometry(QtCore.QRect( 0, -1,  self.inside_02.width(), self.inside_02.height()  ))
+        
+
+
+
+#  system events
 
     def closeEvent(self,event):
         self.super.on_dialog_close()
         print('closed')
 
+    def resizeEvent(self, event):
+        self.responsive_init(event)
 
 
 
 
 
-class Main:
-    def __init__( self ):
+
+
+
+    def event_clicked_button01(self):
+        print('Button pressed')
+        # self.pushButton01.setEnabled(False)
 
         
 
+class Main:
+    def __init__( self ): 
+        # set up database 
         self.set_up_db()
 
 
 
-        
+        # for widget component 
         app = QtWidgets.QApplication(sys.argv) 
+        # assign ui compone to class variable, actual object created and passes this to child class
         self.ui = Ui_Dialog( self )  
+        # get system screen to calculate center , etc.
         self.screen = QtWidgets.QDesktopWidget().screenGeometry()
         
+        # class function to get widget location width and height
         self.update_geometry()
- 
+    
+        #  show the widget
         self.ui.show()
 
+        # update component last widget postion, width and height // initial setup
         self.set_location_()
 
 
 
-
+        # system exit
         sys.exit(app.exec_()) 
 
 
 
+    # move widget to last postion 
     def set_location_ (self):
         Settings = Query()
         db = self.db
@@ -84,11 +263,12 @@ class Main:
 
 
             
-
+    # set widget location and size details to this object
     def update_geometry(self):
         self.dialog = self.ui.geometry()
         
 
+    # execute when widget trigger a close event > triggered from child componet 
     def on_dialog_close (self):
         Settings = Query()
         self.update_geometry()
@@ -113,7 +293,7 @@ class Main:
         self.db.update({ "data": json_string }, Settings.key.search("location"))
 
 
-
+    #  preporcessing the db 
     def set_up_db ( self ):
         try:  
             if not(os.path.exists( DB_PATH ) ):
@@ -124,8 +304,7 @@ class Main:
         self.db = TinyDB(DB_PATH + '/db.json')
 
         
-
-
+    #  function that position the widget to the center of the screen
     def set_center ( self ): 
         screen = self.screen
         dialog = self.dialog
@@ -143,7 +322,7 @@ class Main:
         y = min( max( y, 0 ),s_width - 20 ) 
         self.ui.move(x, y)
 
-
+# main function trigger main class 
 if __name__ == "__main__":
     import sys 
     import os
